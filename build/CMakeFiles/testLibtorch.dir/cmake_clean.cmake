219bd10e7d978a969522184962f2c3a7486ea855
Milan Caban <caban.milan04@gmail.com>
2022-03-04 01:03:18 +0800

    first commit

---
+++ build/CMakeFiles/testLibtorch.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testLibtorch.dir/main.cpp.o"
+  "testLibtorch.pdb"
+  "testLibtorch"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testLibtorch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
